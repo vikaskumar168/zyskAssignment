1034e8610335be44fc690159b092ddf47769e504
vikas <vikaskmr2411@gmail.com>
2025-01-18 01:26:52 +0530

    mobile screen added

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -12,10 +12,7 @@ const Social = () => {
       <div className="w-[375px] h-[260px] px-0 pt-0 pb-[64px] gap-0 bg-white sm:w-[1440px] sm:h-[296px] sm:pt-[96px] sm:pb-[96px] sm:px-0">
         <div className="w-full h-[196px] px-[16px] gap-[32px] sm:w-[1280px] sm:h-[104px] sm:px-[32px] sm:gap-[32px]">
           {/* Text */}
-          <div
-            className="text-[#475467] pl-[240px] sm:w-[1216px] sm:h-[24px] sm:text-center sm:text-[16px] sm:font-medium sm:leading-[24px] sm:text-underline-position-[from-font] sm:text-decoration-skip-ink-none w-[343px] h-[24px]
-           text-center text-[16px] font-medium leading-[24px] text-underline-position-[from-font] text-decoration-skip-ink-none"
-          >
+          <div className=" text-[#475467] sm:pl-[240px] text-center font-medium text-[16px] leading-[24px] sm:w-[1216px] sm:h-[24px] w-[343px] h-[24px]">
             Join 4,000+ companies already growing
           </div>
 
@@ -57,18 +54,42 @@ const Social = () => {
               </div>
 
               {/* Mobile view (3 rows, each containing 2 images) */}
-              <div className="sm:hidden space-y-4">
-                <div className="flex justify-between">
-                  <img src={logo1} alt="Image 1" className="w-1/2" />
-                  <img src={logo2} alt="Image 2" className="w-1/2" />
+              <div className="sm:hidden space-y-4 mt-5">
+                <div className="flex justify-between w-[343px] h-[36px] gap[-32px]">
+                  <img
+                    src={logo1}
+                    alt="Image 1"
+                    className="w-[127.5px] h-[36px] gap-[22px]"
+                  />
+                  <img
+                    src={logo2}
+                    alt="Image 2"
+                    className="w-[125.5px] h-[36px] gap-[22px]"
+                  />
                 </div>
-                <div className="flex justify-between">
-                  <img src={logo3} alt="Image 3" className="w-1/2" />
-                  <img src={logo4} alt="Image 4" className="w-1/2" />
+                <div className="flex justify-between w-[343px] h-[36px] gap[-32px]">
+                  <img
+                    src={logo3}
+                    alt="Image 3"
+                    className="w-[148.5px] h-[36px] gap-[22px]"
+                  />
+                  <img
+                    src={logo4}
+                    alt="Image 4"
+                    className="w-[124.5px] h-[36px] gap-[22px]"
+                  />
                 </div>
-                <div className="flex justify-between">
-                  <img src={logo5} alt="Image 5" className="w-1/2" />
-                  <img src={logo6} alt="Image 6" className="w-1/2" />
+                <div className="flex justify-between w-[343px] h-[36px] gap[-32px]">
+                  <img
+                    src={logo5}
+                    alt="Image 5"
+                    className="w-[147.75px] h-[36px] gap-[22px]"
+                  />
+                  <img
+                    src={logo6}
+                    alt="Image 6"
+                    className="w-[136.5px] h-[36px] gap-[22px]"
+                  />
                 </div>
               </div>
             </div>
